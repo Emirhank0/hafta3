347e038e9a3480caa90395acb131dd51f1e14385
Emirhank0 <48908061+Emirhank0@users.noreply.github.com>
2025-03-03 10:41:54 +0300

    Skor random olarak yapıldı.

---
+++ h3_skor.py
@@ -0,0 +1,14 @@
+import random
+gs=random.randint(0,10)
+print(gs)
+fb=random.randint(0,10)
+print(fb)
+
+if gs>fb:
+    print("GS FB'den dahaçok gol atmıştır.")
+
+elif fb>gs:
+    print("FB GS'den daha fazla gol atmıştır.")
+
+else:
+    print("İkisinin golü eşit")
